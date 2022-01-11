4c8aa8fd1072feebf000760d6cf419ec7b261ae3
pelmeshka <viktoria.pokalo@gmail.com>
2022-01-11 18:35:03 +0300

    Fix

--- z1/Program.cs
+++ z1/Program.cs
@@ -4,9 +4,12 @@
 var LatinChars =  new char [] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
     'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F',
     'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+
 var fileContent = File.ReadAllText("D:\\test.txt");
+
 Console.Write(fileContent);
 Console.WriteLine();
+
 bool IsLatin(char el)
 {
     foreach (var latin in LatinChars)
